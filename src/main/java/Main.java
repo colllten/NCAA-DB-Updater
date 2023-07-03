cbc844ea0de00f5e68eab20d77e9a3a4822c2d7a
Colten Glover <coltenglover@yahoo.com>
2023-07-02 21:57:13 -0400

    Can contact CFDB to get all teams information

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,32 @@
-/**
- * Homework X
- * <p>
- * A program that prints Purdue's Honor Pledge
- *
- * @author Colten Glover, L01
- * @version 01/23/2023
- * <p>
- * Sources: [TA Names]
- */
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        //TODO: change to non-constant number
+        String httpsURL = "https://api.collegefootballdata.com/teams/fbs?year=" + 2022;
+
+        try {
+            URL myURL = new URL(httpsURL);
+            //Open connection to the URL
+            System.out.println("Opening connection...");
+            HttpsURLConnection conn = (HttpsURLConnection) myURL.openConnection();
+            conn.setRequestProperty("Authorization", API_KEYS.CFDB_API);
+
+            System.out.println("Opening streams...");
+            InputStream is = conn.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+
+            //Holds all teams in JSON Array
+            String input = br.readLine();
+
+
+        } catch (Exception e) { //TODO: Specify exception
+
+        }
     }
 }
